e7158bc376932bf795a002bc90052cbe50e21e96
andre <andregranemann@hotmail.com>
2021-12-27 11:09:50 -0300

    criando link painel admin para alteração

--- receitas/admin.py
+++ receitas/admin.py
@@ -3,7 +3,7 @@ from .models import Receita
 
 class ListandoReceitas(admin.ModelAdmin):
     list_display = ('id', 'nome_receita')
-
+    list_display_links = ('id', 'nome_receita')
 
 
 admin.site.register(Receita, ListandoReceitas)
